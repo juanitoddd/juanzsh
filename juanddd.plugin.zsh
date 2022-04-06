dee7823a0e8311d84bd9d76a3da54853cbf60151
Juan server <juan@soyweseed.com>
2022-04-06 23:30:43 +0000

    UPDATE

--- juanddd.plugin.zsh
+++ juanddd.plugin.zsh
@@ -1,15 +1,9 @@
-
-
 # SYSTEM CONF
 
+# HACK TO GROUP CUSTOM ALIAS TOGETHER
 alias juanddd=''
 
-alias come='juanddd ls -ls'
-alias come1='juanddd cd ~'
-alias come2='juanddd pwd'
-alias come3='juanddd echo $HOME'
-
-alias conf="nano ~/.zshrc && source ~/.zshrc"
+alias conf="juanddd nano ~/.zshrc && source ~/.zshrc"
 alias ports1='netstat -ntap'
 alias ports2='netstat -ntl'
 alias ports_pid='netstat -lp --inet'
@@ -19,20 +13,22 @@ alias top_ram='top -o %MEM'
 alias wanip='dig +short myip.opendns.com @resolver1.opendns.com'
 
 # DOCKER
-alias doco="docker-compose"
-alias dctools="docker-compose up -d"
-alias dcserv="docker-compose -f compose.yml up -d"
-alias dps='docker ps --format="table {{.Names}}\t{{.Image}}\t{{.Status}}"'
-alias rmexit="docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm"
+alias doco="juanddd docker-compose"
+alias docot="juanddd doco -f compose.tools.yml"
+alias docob="juanddd doco -f compose.base.yml"
+alias docos="juanddd doco -f compose.services.yml"
+alias docor="juanddd doco -f compose.routes.yml"
+alias docof="juanddd doco -f compose.frontend.yml"
+alias rmexit="juanddd docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs docker rm"
 
 # SOYWESEED
-alias home="cd /home/soyweseed/"
+alias home="juanddd cd /home/soyweseed/"
 
 # SYSTEM
-alias c="clear"
-alias sshkey="cat ~/.ssh/id_rsa.pub"
+alias c="juanddd clear"
+alias sshkey="juanddd cat ~/.ssh/id_rsa.pub"
 
-alias devgroup="sudo chgrp -R developers /home/soyweseed/staging"
+alias devgroup="juanddd sudo chgrp -R developers /home/soyweseed/staging"
 
 # GIT
 alias gits="git status"
